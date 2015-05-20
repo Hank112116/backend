2e699b3ea7b9c4b297ccd0e26ec11e454954089e
Jaster <jaster.chung@hwtrek.com>
2015-05-20 18:06:03 +0800

    change access:tarball schedule to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-        $schedule->command('access:tarball')->everyFiveMinutes();
+        $schedule->command('access:tarball')->daily();
 	}
 
 }
