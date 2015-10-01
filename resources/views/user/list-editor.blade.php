11ebf2edb5cdbf3709fe1336a95e228549d7289a
Sylvia <sylvia.shen@hwtrek.com>
2015-10-01 10:50:05 +0800

    [FIX] remove redundant code

--- resources/views/user/list-editor.blade.php
+++ resources/views/user/list-editor.blade.php
@@ -27,13 +27,7 @@
                 <tr>
                     <th>#</th>
                     <th>Name</th>
-                    @if(Auth::user()->isAdmin())
-                        <th>Internal PM</th>
-                    @endif
                     <th>Type</th>
-                    @if(Auth::user()->isManagerHead() || Auth::user()->isAdmin())
-                        <th class="table--user-mail">EMail</th>
-                    @endif
                     <th></th>
                 </tr>
 
@@ -44,32 +38,7 @@
                             <a href="{!! $user->textFrontLink() !!}" target="_blank">
                                 {{ $user->textFullName() }}</a>
                         </td>
-                        @if(Auth::user()->isAdmin())
-                            <td>{!! $user->textHWTrekPM() !!}<br>
-                                @if(!$user->isHWTrekPM())
-                                    <span class="user-sub-category">
-                                <input type="checkbox"  class="change_pm" rel="{!! $user->user_id !!}"> To PM
-                            </span>
-                                @else
-                                    <span class="user-sub-category">
-                                <input type="checkbox"  class="change_user" rel="{!! $user->user_id !!}"> To User
-                            </span>
-                                @endif
-                            </td>
-                        @endif
-
                         <td>{!! $user->textType() !!}</td>
-                        @if(Auth::user()->isManagerHead() || Auth::user()->isAdmin())
-                            <td class="table--user-mail">
-                                {{ $user->email }}
-                                @if('facebook' === $user->social)
-                                    <i class="fa fa-facebook-square"></i>
-                                @elseif('linkedin' === $user->social)
-                                    <i class="fa fa-linkedin-square"></i>
-                                @endif
-                            </td>
-                        @endif
-
                         <td>
                             {!! link_to_action(
                                     'UserController@showDetail', 'DETAIL',
