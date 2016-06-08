046d04a5f705773beb6ba5f7f4a97643e0b4009e
Hank <hank.chang@hwtrek.com>
2016-06-08 10:02:15 +0800

    [FIXED] Fix editor role browse member detail view error

--- resources/views/user/detail-editor.blade.php
+++ resources/views/user/detail-editor.blade.php
@@ -81,7 +81,7 @@
 
         </div>
     </div>
-    @if ($attachments)
+    @if ($attachments != 'null')
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
